db3908d594f2128839f93a6c207a99f2b5a6dd9d
Oliver Tan <otan@cockroachlabs.com>
2023-02-27 13:28:42 +1100

    add tests for mysql dsn conv

--- pkg/dbconn/mysql.go
+++ pkg/dbconn/mysql.go
@@ -18,8 +18,18 @@ type MySQLConn struct {
 }
 
 func ConnectMySQL(ctx context.Context, id ID, u *url.URL) (*MySQLConn, error) {
+	db, err := sql.Open("mysql", mysqlURLToDSN(u))
+	if err != nil {
+		return nil, err
+	}
+	return &MySQLConn{id: id, u: u, DB: db, typeMap: pgtype.NewMap()}, nil
+}
+
+func mysqlURLToDSN(u *url.URL) string {
+	// TODO: TLS and the like
 	dsn := &mysql.Config{
 		Addr:   u.Host,
+		Net:    "tcp",
 		User:   "root",
 		DBName: strings.TrimLeft(u.Path, "/"),
 	}
@@ -29,12 +39,7 @@ func ConnectMySQL(ctx context.Context, id ID, u *url.URL) (*MySQLConn, error) {
 			dsn.Passwd = p
 		}
 	}
-	// TODO: TLS and the like
-	db, err := sql.Open("mysql", dsn.FormatDSN())
-	if err != nil {
-		return nil, err
-	}
-	return &MySQLConn{id: id, u: u, DB: db, typeMap: pgtype.NewMap()}, nil
+	return dsn.FormatDSN()
 }
 
 func (c *MySQLConn) ID() ID {
---
+++ pkg/dbconn/mysql_test.go
@@ -0,0 +1,26 @@
+package dbconn
+
+import (
+	"net/url"
+	"testing"
+
+	"github.com/cockroachdb/datadriven"
+	"github.com/pkg/errors"
+)
+
+func TestMySQLURLToDSN(t *testing.T) {
+	datadriven.Walk(t, "testdata/dsn", func(t *testing.T, path string) {
+		datadriven.RunTest(t, path, func(t *testing.T, d *datadriven.TestData) string {
+			switch d.Cmd {
+			case "mysql":
+				u, err := url.Parse(d.Input)
+				if err != nil {
+					t.Fatal(errors.Wrapf(err, "error converting %s to url", d.Input))
+				}
+				return mysqlURLToDSN(u)
+			}
+			t.Fatalf("unknown command %s", d.Cmd)
+			return ""
+		})
+	})
+}
